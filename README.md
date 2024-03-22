92e974397eeb119e5faaf6c549fe27594ade602c
Ugo <ugwadoux@gmail.com>
2024-03-22 10:26:11 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-# Installation du projet
+# Green hub (Front)
+
+![image]({https://img.shields.io/badge/Vue%20js-35495E?style=for-the-badge&logo=vuedotjs&logoColor=4FC08D})
+
+## Description
+
+### What is Green hub   ?
+
+Green hub is a company specializing in selling eco-friendly products and services. This project is the front-end part of the e-commerce website. In this website you can command product, services and also a blog part to review product, eco-friendly tutorial etc ...
+
+Professional can also provide services through the platform
+
+
+### What are the technologies used ?
+
+The frontend part is develop in Vue.js.
+
+### Plugins used :
+
+- unplugin for auto-import icons (using phosphore icons library)
+- axios for Http request
+- tailwind for css
+- daisyUI for component css
+
+## Installation
 
 ```sh
 npm install
@@ -12,6 +36,3 @@ npm run dev
 
 Plugins installés :
 - unplugin avec phosphore
-- axios
-- tailwind
-- daisyUI
--- src/views/product/ProductView.vue
+++ src/views/product/ProductView.vue
@@ -129,26 +129,26 @@ const cartStore = useCartStore()
             </div>
             <div class="mb-6">
               <h2 class="mb-2 text-lg font-bold text-gray-700 dark:text-gray-400">Description :</h2>
-<!--              <div class="bg-gray-100 dark:bg-gray-700 rounded-xl">-->
-                <div class="p-3 lg:p-5 ">
-<!--                  <div class="p-2 rounded-xl lg:p-6 dark:bg-gray-800 bg-gray-50">-->
-<!--                    <div class="flx   gap-x-10 gap-y-4">-->
-<!--                      <div class="w-full mb-4 md:w-2/5">-->
-                        <div class="flex ">
+              <!--              <div class="bg-gray-100 dark:bg-gray-700 rounded-xl">-->
+              <div class="p-3 lg:p-5 ">
+                <!--                  <div class="p-2 rounded-xl lg:p-6 dark:bg-gray-800 bg-gray-50">-->
+                <!--                    <div class="flx   gap-x-10 gap-y-4">-->
+                <!--                      <div class="w-full mb-4 md:w-2/5">-->
+                <div class="flex ">
 <span class="mr-3 text-gray-500 dark:text-gray-400">
 </span>
-                          <div>
+                  <div>
 
-                            <h2 class="text-base font-semibold text-gray-700 dark:text-gray-400">
-                              {{ product?.description }}
-                            </h2>
-<!--                          </div>-->
-<!--                        </div>-->
-<!--                      </div>-->
+                    <h2 class="text-base font-semibold text-gray-700 dark:text-gray-400">
+                      {{ product?.description }}
+                    </h2>
+                    <!--                          </div>-->
+                    <!--                        </div>-->
+                    <!--                      </div>-->
 
-                    </div>
                   </div>
-<!--                </div>-->
+                </div>
+                <!--                </div>-->
               </div>
             </div>
             <div class="py-6 mb-6 border-t border-b border-gray-200 dark:border-gray-700">
